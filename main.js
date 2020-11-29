78a102b01b69bbed7754cf399f6e113d07715c38
randy clinton <rclinton@rcanalytics.com>
2020-11-29 18:47:54 -0500

    solving linting issues

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var CELL_SHADDED = Math.floor(Math.random() * 9) + 1;
-var CLICKS_THRESHOLD = Math.floor(Math.random() * 5) + 10;
-var CLICKS = 0;
-var DELAY_MS = 500;
+const CELL_SHADDED = Math.floor(Math.random() * 9) + 1;
+const CLICKS_THRESHOLD = Math.floor(Math.random() * 5) + 10;
+const CLICKS = 0;
+const DELAY_MS = 500;
 
 console.log('init cell....' + CELL_SHADDED);
 
 function findNextCell() {
-    var newCell =  Math.floor(Math.random() * 9) + 1;
+    const newCell = Math.floor(Math.random() * 9) + 1;
     if (newCell === CELL_SHADDED) {
         newCell = findNextCell()
     }
@@ -28,9 +28,9 @@ async function toggleShadding(e) {
         console.log("sleep");
         await new Promise(r => setTimeout(r, DELAY_MS));
     }
-    
-    var currentShadding = e.target.classList.value;
-    var cellNumber = e.target.textContent
+
+    const currentShadding = e.target.classList.value;
+    const cellNumber = e.target.textContent
     if (currentShadding === 'cell-not-shadded') {
         console.log('do nothing; cell ' + cellNumber + ' clicked');
     } else if (currentShadding === 'cell-shadded') {
@@ -40,7 +40,7 @@ async function toggleShadding(e) {
         CELL_SHADDED = findNextCell();
         // console.log(nextCell, )
         toggleOnCell(document.getElementById('grid-element-' + CELL_SHADDED));
-        
+
     } else {
         console.warn('no class here');
     }
@@ -49,12 +49,12 @@ async function toggleShadding(e) {
 }
 
 
-var arr = Array.from(Array(10).keys());
+const arr = Array.from(Array(10).keys());
 arr.splice(0, 1);
 
 arr.map(el => {
     console.log(el);
-    var _cell = document.getElementById('grid-element-' + el);
+    const _cell = document.getElementById('grid-element-' + el);
     _cell.addEventListener("click", toggleShadding);
     if (el === CELL_SHADDED) {
         _cell.classList.remove('cell-not-shadded');
